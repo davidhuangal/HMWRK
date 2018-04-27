66b4d0994ad397c759a136e645b5cb59aca47efc
Josh <jdbb7d@mail.missouri.edu>
2018-04-27 12:25:42 -0500

    Added new class button and function

--- src/hmwrk/ClassListController.java
+++ src/hmwrk/ClassListController.java
@@ -7,11 +7,14 @@ package hmwrk;
 
 import com.jfoenix.controls.JFXListView;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextInputDialog;
 
 /**
  * FXML Controller class
@@ -27,6 +30,16 @@ public class ClassListController extends Switchable implements Initializable {
     //These names will come from a JSON file, but are currently set to test values
     //just to see how it works.
     ObservableList<String> list = FXCollections.observableArrayList("Classes", "Go", "Here");
+    @FXML
+    private void addNewClass(ActionEvent event){
+        TextInputDialog dialog = new TextInputDialog("");
+        dialog.setTitle("New Class");
+        dialog.setHeaderText("Enter your new class name");
+        Optional<String> name = dialog.showAndWait();
+        if(name.isPresent()){
+            list.add(name.get());
+        }
+    }
 
     /**
      * Initializes the controller class.
